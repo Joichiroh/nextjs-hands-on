d3fcbb778cb7937bbf872450dcfad9c8e3a89546
Joichiroh <joichiroh.inafuku@gmail.com>
2021-09-06 18:06:26 +0900

    theme color changes

--- styles/theme.js
+++ styles/theme.js
@@ -1,6 +1,7 @@
 import { createTheme } from '@material-ui/core/styles';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import red from '@material-ui/core/colors/red';
+import { blueGrey } from '@material-ui/core/colors';
 
 export const palette = {
 };
@@ -8,7 +9,7 @@ export const palette = {
 const theme = createTheme({
   palette: {
     primary: deepPurple,
-    secondary: red,
+    secondary: blueGrey,
   },
 });
 
